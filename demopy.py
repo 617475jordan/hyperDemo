139292d5ca3b65a547289817b48ce12ed24993db
617475jordan <617475jordan@sina.com>
2018-01-24 17:33:13 +0800

    Update demopy.py

--- demopy.py
+++ demopy.py
@@ -12,7 +12,8 @@ def cv_imread(img_path):
     return cv_img
 
 if __name__ == '__main__':
-    FindPath = 'license//湘'
+    '''
+    FindPath = 'F://NI//Materials//license//2018.1.24'
 
     FileNames = os.listdir(FindPath)
     count=0
@@ -41,3 +42,29 @@ if __name__ == '__main__':
     print('result:%.1f'%(success/count*100))
     print('success:%d'%success)
     print ('total image is:%d'%count)
+    '''
+    cap = cv2.VideoCapture('video//bandicam0.mp4')
+    while(1):
+       t0 = time.time()
+       # get a frame
+       ret, frame = cap.read()
+
+       # show a frame
+       #cv2.imshow("capture", frame)
+       if cv2.waitKey(100) & 0xFF == ord('q'):
+          break
+       sp = frame.shape
+       if sp[0]==0|sp[1]==0|sp[2]==0:
+           break
+       sz1 = sp[0]#height(rows) of image
+       sz2 = sp[1]#width(colums) of image
+        #print(sz1,sz2)
+       image,res,flag  = pp.SimpleRecognizePlate(frame,"capture")
+       out=image
+       if(sz2>1280):
+            out=cv2.resize(image,(int(sz2*2.0/4.0),int(sz1*2.0/4.0)),interpolation=cv2.INTER_AREA)
+       print(time.time() - t0,"s")
+       cv2.imshow('current Result',out)
+       cv2.waitKey(1)
+    cap.release()
+    cv2.destroyAllWindows()
