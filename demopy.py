951a080ecce03a24cd62565911131aa679178390
617475jordan <617475jordan@sina.com>
2018-01-25 18:57:18 +0800

    Update demopy.py

--- demopy.py
+++ demopy.py
@@ -12,13 +12,15 @@ def cv_imread(img_path):
     return cv_img
 
 if __name__ == '__main__':
-    '''
-    FindPath = 'F://NI//Materials//license//2018.1.24'
+
+    FindPath = 'F://NI//Materials//license//“青”牌-36'
 
     FileNames = os.listdir(FindPath)
     count=0
     success=0
+
     for file_name in FileNames:
+
         t0 = time.time()
         fullfilename=os.path.join(FindPath,file_name)
         #print(file_name)
@@ -27,16 +29,19 @@ if __name__ == '__main__':
 
         sz1 = sp[0]#height(rows) of image
         sz2 = sp[1]#width(colums) of image
+        if(sz2>=1024):
+            image=cv2.resize(image,(int(sz2*2.0/4.0),int(sz1*2.0/4.0)),interpolation=cv2.INTER_AREA)
         #print(sz1,sz2)
-        image,res,flag  = pp.SimpleRecognizePlate(image,file_name)
+        result =[[0 for col in range(2)] for row in range(5)]
+        #image,res=pp.SimpleRecognizePlateByE2E(image)
+        image,res,flag,result  = pp.SimpleRecognizePlate(image,file_name)
+        print(result)
         if flag==1:
             success=success+1
-        out=image
-        if(sz2>1280):
-            out=cv2.resize(image,(int(sz2*2.0/4.0),int(sz1*2.0/4.0)),interpolation=cv2.INTER_AREA)
         count = count + 1
+
         print(time.time() - t0,"s")
-        cv2.imshow(str(count)+'.jpg',out)
+        cv2.imshow(str(count)+'.jpg',image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     print('result:%.1f'%(success/count*100))
@@ -58,13 +63,17 @@ if __name__ == '__main__':
            break
        sz1 = sp[0]#height(rows) of image
        sz2 = sp[1]#width(colums) of image
-        #print(sz1,sz2)
-       image,res,flag  = pp.SimpleRecognizePlate(frame,"capture")
-       out=image
        if(sz2>1280):
-            out=cv2.resize(image,(int(sz2*2.0/4.0),int(sz1*2.0/4.0)),interpolation=cv2.INTER_AREA)
+          image=cv2.resize(image,(int(sz2*2.0/4.0),int(sz1*2.0/4.0)),interpolation=cv2.INTER_AREA)
+        #print(sz1,sz2)
+       result =[[0 for col in range(2)] for row in range(5)]
+       #image,res=pp.SimpleRecognizePlateByE2E(image)
+       image,res,flag,result  = pp.SimpleRecognizePlate(frame,'capture')
+       print(result)
        print(time.time() - t0,"s")
-       cv2.imshow('current Result',out)
+       cv2.imshow('current Result',image)
        cv2.waitKey(1)
+
     cap.release()
     cv2.destroyAllWindows()
+   '''
